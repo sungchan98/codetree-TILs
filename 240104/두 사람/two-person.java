fd37b82b96d81a7ca8d0460029c3ecbb12872b32
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-04 09:52:19 +0900

    Title: 두 사람, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240104/두 사람/two-person.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+    int a1 = sc.nextInt();
+    String a2 = sc.next();
+    int b1 = sc.nextInt();
+    String b2 = sc.next();
+
+    if ((a1 >= 19 && a2.equals("M")) || (b1 >= 19 && b2.equals("M"))) {
+      System.out.println("1");
+    } else {
+      System.out.println("0");
+    }
+  }
+}
