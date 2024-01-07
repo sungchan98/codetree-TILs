6a87797ed131594add7130ffe5148955cf433ae3
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-07 20:38:21 +0900

    Title: 중앙값 구하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240107/중앙값 구하기/find-the-median.java
+++ 240107/중앙값 구하기/find-the-median.java
@@ -8,11 +8,11 @@ public class Main {
     int b = sc.nextInt();
     int c = sc.nextInt();
 
-    if (a > b && a < c) {
+    if ((a > b && a < c) || (a > c && a < b)) {
       System.out.println(a);
-    } else if (b > a && b < c) {
+    } else if ((b > a && b < c) || (b > c && b < a)) {
       System.out.println(b);
-    } else if (c > a && c < b) {
+    } else if ((c > a && c < b) || (c > b && c < a)) {
       System.out.println(c);
     }
   }
