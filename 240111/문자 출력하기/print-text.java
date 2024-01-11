784f3598860569df86fc49771d128d733c7f2e2d
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-11 09:26:27 +0900

    Title: 문자 출력하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240111/문자 출력하기/print-text.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        String n = sc.next();
+
+        for(int i = 1; i <= 8; i++){
+            System.out.print(n);
+        }
+    }
+}
