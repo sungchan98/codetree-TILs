22f7295f61d0fe0b063aa761d75f921c7f44f977
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-13 14:01:01 +0900

    Title: a 부터 b 까지, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240113/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int i = 0;
+
+    for (i = a; i <= b;) {
+      if (i % 2 != 0) {
+        System.out.print(i + " ");
+        i *= 2;
+
+      } else if (i % 2 == 0) {
+        System.out.print(i + " ");
+        i += 3;
+      }
+    }
+  }
+}
