65062b53ce4a60a2fe7aa227887bc67be36cdbe2
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-14 21:00:46 +0900

    Title: 배수의 개수 2, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240114/배수의 개수 2/number-of-multipliers-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.\\
+        Scanner sc = new Scanner(System.in);
+        
+        int cnt = 0;
+
+        for(int i=1;i<=10;i++){
+            int a= sc.nextInt();
+            if(a % 2 != 0){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
