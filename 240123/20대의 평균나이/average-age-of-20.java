c15fdca9c7c9482ffca8492d4a6c4c82d5b8c208
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-23 14:22:59 +0900

    Title: 20대의 평균나이, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240123/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+    int cnt = 0;
+    int sum = 0;
+    while (true) {
+      int age = sc.nextInt();
+
+      if (age >= 20 && age <= 29) {
+        sum += age;
+        cnt++;
+      }
+      if (!(age >= 20 && age <= 29)) {
+        System.out.printf("%.2f", (double) sum / cnt);
+        break;
+      }
+    }
+  }
+}
