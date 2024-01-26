a4c8643cc3462e92e4997636debbe6d1b1bdc3a0
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-26 18:27:43 +0900

    Title: 합성수 판별기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/합성수 판별기/factorization-discriminator.java
+++ 240126/합성수 판별기/factorization-discriminator.java
@@ -7,7 +7,7 @@ public class Main {
     int n = sc.nextInt();
     boolean satisfied = false;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 2; i < n; i++) {
       if (n % i == 0) {
         satisfied = true;
       }
