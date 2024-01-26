765d630b6cf66edf2ac8afd1a6d32bccc73ca582
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-26 18:45:50 +0900

    Title: ab 사이에 없는 c, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240126/ab 사이에 없는 c/c-between-a-and-b-2.java
+++ 240126/ab 사이에 없는 c/c-between-a-and-b-2.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        boolean satisfied = true;
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    boolean satisfied = false;
 
-        for(int i=a; i<=b;i++){
-            if(c%i =! 0){
-                satisfied = false;
-            }
-        }
-
-        if(satisfied == true) {
-            System.out.println("YES");
-        }else{
-            System.out.println("NO");
-        }
+    for (int i = a; i <= b; i++) {
+      if (i % c == 0) {
+        satisfied = true; // c의 배수가 있다
+      }
     }
+
+    if (satisfied == false) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
 }
