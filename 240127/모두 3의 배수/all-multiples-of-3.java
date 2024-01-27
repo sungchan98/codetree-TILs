b75826dbfb994771dbcab4ec6029310617cb9dfc
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-27 17:41:07 +0900

    Title: 모두 3의 배수, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 240127/모두 3의 배수/all-multiples-of-3.java
+++ 240127/모두 3의 배수/all-multiples-of-3.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        boolean satisfied = false;
-        for(int i =0; i<=4; i++) {
-            int a = sc.nextInt();
-            if(a % 3 == 0){
-                satisfied = true;
-            }
-        }
-        if(satisfied == true) {
-            System.out.println("1");
-        }else {
-            System.out.println("0");
-        }
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+    boolean satisfied = true;
+    for (int i = 0; i <= 4; i++) {
+      int a = sc.nextInt();
+      if (a % 3 != 0) {
+        satisfied = false;
+      }
     }
+    if (satisfied == true) {
+      System.out.println("1");
+    } else {
+      System.out.println("0");
+    }
+  }
 }
