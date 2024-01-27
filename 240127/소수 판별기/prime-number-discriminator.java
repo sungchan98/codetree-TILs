2b05acdf060e74246a961dc5b816806de92b9ac6
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-27 17:29:00 +0900

    Title: 소수 판별기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 240127/소수 판별기/prime-number-discriminator.java
+++ 240127/소수 판별기/prime-number-discriminator.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        boolean a = false;
-        for(int i=2; i <=1000; i++){
-            if(i % n == 0){
-                a = true;
-            }
-        }
-        if(a == true) {
-            System.out.println("P");
-        }else {
-            System.out.println("C");
-        }
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    boolean a = true;
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
+        a = false;
+      }
     }
+    if (a == true) {
+      System.out.println("P");
+    } else {
+      System.out.println("C");
+    }
+  }
 }
