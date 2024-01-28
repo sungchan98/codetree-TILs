1916ebb3de02d72e3d81f5cb0d7f20c008cd9fd6
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-01-28 22:10:11 +0900

    Title: 별표 출력하기 2, Time: 234ms, Memory: 10MB, Status: Passed - CodeTree

--- 240128/별표 출력하기 2/print-star-2.java
+++ 240128/별표 출력하기 2/print-star-2.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
 
-        for(int i=0; i<n; i++){
-            for(int j=n; j>=i; j--){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+    for (int i = 0; i < n; i++) {
+      for (int j = n; j > i; j--) {
+        System.out.print("* ");
+      }
+      System.out.println();
     }
+  }
 }
