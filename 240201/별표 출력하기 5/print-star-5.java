c43d4fa5871186f8b081f139cbaa560b5051a918
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-02-01 10:06:33 +0900

    Title: 별표 출력하기 5, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240201/별표 출력하기 5/print-star-5.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int cnt = n;
+
+    for (int i = n; i > 0; i--) {
+      for (int j = 1; j <= i * i; j++) {
+        System.out.print("*");
+        if (j % i == 0) {
+          System.out.print(" ");
+        }
+      }
+      System.out.println();
+    }
+  }
+}
