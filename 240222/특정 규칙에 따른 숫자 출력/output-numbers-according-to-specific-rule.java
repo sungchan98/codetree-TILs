b8033bde33e058bf3a98555f13ff8198cf286dde
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-02-22 10:07:59 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240222/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("  ");
+            }
+            for(int j=n;j>i;j--){
+                if(cnt == 10){
+                    cnt=1;
+                }
+                System.out.print(cnt++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
