e0eb02902c31b49fc242f3ad6c8ab97dd1b031f1
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-03-02 13:56:06 +0900

    Title: 학점 계산기, Time: 154ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240302/학점 계산기/credit-calculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        double[] arr = new double[n];
+        double sum = 0;
+        int cnt = 0;
+        for(int i=0; i<n;i++){
+            cnt++;  
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+        if(sum/n >= 4.0){
+            System.out.printf("%.1f\n",(double)sum/n);
+             System.out.println("perfect");
+        } else if(sum/n >= 3.0)  {
+            System.out.printf("%.1f\n",(double)sum/n);
+            System.out.println("Good");
+        }  else{
+            System.out.printf("%.1f\n",(double)sum/n);
+            System.out.println("Poor");
+        }   
+    }
+}
