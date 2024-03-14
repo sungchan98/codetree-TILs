27600739f038dba31d11a64a1a8061629ced0d6b
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-03-14 09:35:33 +0900

    Title: 1-9 개수 세기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240314/1-9 개수 세기/count-one-to-nine.java
+++ 240314/1-9 개수 세기/count-one-to-nine.java
@@ -5,8 +5,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int arr[] = new int[10];
-        int cntArr[] = new int[10];    
+        int arr[] = new int[n];
+        int cntArr[] = new int[n];    
         
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
