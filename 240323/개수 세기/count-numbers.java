1cfe184b7335eb927d44f06b0ee2edcd02c0243e
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-03-23 23:45:11 +0900

    Title: 개수 세기, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240323/개수 세기/count-numbers.java
+++ 240323/개수 세기/count-numbers.java
@@ -4,11 +4,13 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int[] n = sc.nextInt();
+        int n = sc.nextInt();
         int m = sc.nextInt();
-       
+        int arr[] = new int[n];
+        int cnt=0;
         for(int i=0;i<n;i++){
-            if(n[i] == m ){
+            arr[i] = sc.nextInt();
+            if(arr[i] == m ){
                 cnt++;
             }
         }
