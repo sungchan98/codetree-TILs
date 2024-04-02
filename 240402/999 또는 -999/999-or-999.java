1dfc4131fa30913ac6ab766843c36b0c87d8e2ed
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-04-02 09:26:33 +0900

    Title: 999 또는 -999, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/999 또는 -999/999-or-999.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[100];
+        final int INT_MAX = Integer.MAX_VALUE;
+        final int INT_MIN = Integer.MIN_VALUE;
+
+        int maxVal = INT_MIN;
+        int minVal = INT_MAX;
+
+        for(int i=0;i<100;i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] == 999 || arr[i] == -999){
+                break;
+            }
+            if (minVal > arr[i]) {
+                minVal = arr[i];
+            }
+            if (maxVal < arr[i]) {
+                maxVal = arr[i];
+            }
+        }
+        System.out.printf(maxVal + " " + minVal);
+    }
+}
