8955d25d2ec400e2e7cf1519bc9160f2fd88799f
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-04-10 13:51:05 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240410/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
+++ 240410/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -5,8 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        
-
+    
         int arr[] = new int[100];
         int cnt=0;
         int idx = -1;
@@ -16,9 +15,11 @@ public class Main {
                 cnt++;
             }
             if(cnt==3){
-                idx=i+1;
+                idx=i;
+                break;
             }
         }
-        System.out.println(idx);
+        //System.out.println(cnt);
+        System.out.println(idx+1);
     }
 }
