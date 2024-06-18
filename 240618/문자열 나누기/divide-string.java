43c0fc4e5fca2a93b1ee137eb410bde1b62baf46
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-06-18 21:53:07 +0900

    Title: 문자열 나누기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240618/문자열 나누기/divide-string.java
@@ -0,0 +1,31 @@
+package algorithm.test.codetree;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    // 여기에 코드를 작성해주세요.
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    String[] str = new String[n];
+    String strAll = "";
+
+    for (int i = 0; i < n; i++) {
+      str[i] = sc.next();
+    }
+
+    for (int i = 0; i < n; i++) {
+      strAll += str[i];
+    }
+
+    for (int i = 0; i < strAll.length(); i++) {
+      System.out.print(strAll.charAt(i));
+      if ((i + 1) % 5 == 0) {
+        System.out.println();
+      }
+    }
+
+
+  }
+}
