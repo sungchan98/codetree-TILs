a99b8643f9e08d925d1b9e9c9da9b38dd735da88
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-07-02 19:05:20 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 313ms, Memory: 21MB, Status: Passed - CodeTree

--- 240702/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 240702/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -1,35 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    // 여기에 코드를 작성해주세요.
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    String s = sc.next();
-    char[] arr = s.toCharArray();
-    int q = sc.nextInt();
+        String s = sc.next();
+        int q = sc.nextInt();
 
-
-    for (int i = 0; i < q; i++) {
-      int a = sc.nextInt();
-      if (a == 1) {
-        s = s.substring(1, s.length()) + s.substring(0, 1);
-        System.out.println(s);
-      } else if (a == 2) {
-        s = s.substring(s.length() - 1, s.length()) + s.substring(0, s.length() - 1);
-        System.out.println(s);
-      } else if (a == 3) {
-
-        for (int j = 0; j < s.length() / 2; j++) {
-          // s += s.substring(s.length() - j - 1, s.length() - j);
-          char temp = arr[j];
-          arr[j] = s.charAt(s.length() - 1 - j);
-          arr[s.length() - 1 - j] = temp;
+        for (int i = 0; i < q; i++) {
+            int a = sc.nextInt();
+            if (a == 1) {
+                // 왼쪽으로 한 칸 회전
+                s = s.substring(1) + s.charAt(0);
+                System.out.println(s);
+            } else if (a == 2) {
+                // 오른쪽으로 한 칸 회전
+                s = s.charAt(s.length() - 1) + s.substring(0, s.length() - 1);
+                System.out.println(s);
+            } else if (a == 3) {
+                // 문자열 뒤집기
+                StringBuilder sb = new StringBuilder(s);
+                s = sb.reverse().toString();
+                System.out.println(s);
+            }
         }
-        s = new String(arr);
-        System.out.println(s.toString());
-
-      }
+        sc.close();
     }
-  }
 }
