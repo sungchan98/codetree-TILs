2503203f21a2b9ec79238dd064b0b2fdcd6e8024
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-07-04 03:19:44 +0900

    Title: 아스키코드의 합과 차, Time: 103ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240704/아스키코드의 합과 차/sum-and-subtract-in-ASCII.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        char b = sc.next().charAt(0);
+
+        System.out.print((int)a + (int)b + " ");
+        
+        if(a>b){
+            System.out.print((int)a - (int)b );
+        }else{
+            System.out.print((int)b - (int)a );
+    }
+}
+}
