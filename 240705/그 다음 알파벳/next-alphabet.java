9e23d3847fc06cc4eff02667bd6daa362c8b4bd0
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-07-05 16:12:54 +0900

    Title: 그 다음 알파벳, Time: 109ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240705/그 다음 알파벳/next-alphabet.java
+++ 240705/그 다음 알파벳/next-alphabet.java
@@ -5,9 +5,9 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         char c = sc.next().charAt(0);
-        int c2 = (int)c + 1;
-        if((int)c2==122){
-            c=97;
+        int c2 = (int)c;
+        if(c2 + 1 == 123){
+            c2=97;
         }
         System.out.println((char)c2);
     }
