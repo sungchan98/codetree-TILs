f898e92b58bbb75dbe227f3a1f12ac6f9de17756
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-07-08 21:05:39 +0900

    Title: 소문자와 숫자, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240708/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // 문자열 입력
+        String str = sc.nextLine();
+
+        // 결과를 저장할 StringBuilder
+        StringBuilder result = new StringBuilder();
+
+        // 문자열의 각 문자를 순회
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                result.append((char)(c + ('a' - 'A')));
+            } else if (c >= 'a' && c <= 'z') {
+                result.append(c);
+            }else if(c >= '0' && c <= '9'){
+ result.append(c);
+            }
+        }
+
+
+        System.out.println(result.toString());
+        
+
+    }
+}
