9d3f8956613798891747505628faa5d239bb79f6
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-07-11 17:03:06 +0900

    Title: 붙여서 합하기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240711/붙여서 합하기/add-and-add.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(Integer.parseInt(a+b) + Integer.parseInt(b+a));
+    }
+}
