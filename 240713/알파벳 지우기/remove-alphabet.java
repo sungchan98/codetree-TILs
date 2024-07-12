daf4a0c8af13cbb2203fffeefe5e7fcb3eb8db64
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-07-13 00:23:06 +0900

    Title: 알파벳 지우기, Time: 113ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240713/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+        String a2 = "";
+        String b2 = "";
+        for(int i = 0; i < a.length(); i++) {
+            if(a.charAt(i) >= '0' && a.charAt(i) <= '9'){
+                a2 += a.charAt(i);
+            } 
+        }
+
+        for(int i = 0; i < b.length(); i++) {
+            if(b.charAt(i) >= '0' && b.charAt(i) <= '9'){
+                b2 += (b.charAt(i) - '0');
+            } 
+        }
+
+        System.out.println(Integer.parseInt(a2) + Integer.parseInt(b2));
+
+
+    }
+}
