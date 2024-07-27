c3f0d1dceff38de674e4e5041f465e2b236bc846
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-07-27 23:41:47 +0900

    Title: 소수 구하기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240727/소수 구하기/get-prime.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.print("2 ");
+        for(int i=2; i<=n;i++){
+            int cnt = 0;
+            for(int j=1; j<=i; j++){
+                if(i%j==0){
+                    cnt++;
+                    if(cnt==2 && j != 2){
+                        System.out.print(j + " ");
+                    }
+                }
+            }
+        }
+    }
+}
