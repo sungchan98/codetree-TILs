a6421fac66248a137d86b8b4269962dc0202eeab
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-08-27 16:27:00 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 219ms, Memory: 13MB, Status: Passed - CodeTree

--- 240827/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
+++ 240827/가장 왼쪽에 있는 최댓값/leftmost-max-value.java
@@ -5,7 +5,7 @@ public class Main {
     Scanner sc = new Scanner(System.in);
 
     int n = sc.nextInt();
-    int arr[] = new int[n];
+    int arr[] = new int[n + 1];
 
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
@@ -15,13 +15,13 @@ public class Main {
       int maxVal = 0;
       int idx = 0;
 
-      for (int i = 0; i < n; i++) {
+      for (int i = 0; i <= n; i++) {
         if (arr[i] >= maxVal) {
           maxVal = arr[i];
         }
       }
 
-      for (int i = 0; i < n; i++) {
+      for (int i = 0; i <= n; i++) {
         if (arr[i] == maxVal) {
           idx = i;
           break;
