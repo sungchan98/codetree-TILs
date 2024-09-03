e344f1ac7d2efc9194de2fe5b6af7212fd1dfd6c
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-09-03 19:27:42 +0900

    Title: 대각선으로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240903/대각선으로 숫자 채우기/diagonal-numbering.java
@@ -0,0 +1,36 @@
+package algorithm.test.baekjoon.level04.ex06;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+
+    int[][] arr2d = new int[n][m];
+
+    int num = 1;
+    int sum = 0;
+
+    for (int k = 0; k <= n + m; k++) {
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          if (i + j == k) {
+            arr2d[i][j] = num;
+            num++;
+          }
+        }
+      }
+    }
+
+    // 출력
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.print(arr2d[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
