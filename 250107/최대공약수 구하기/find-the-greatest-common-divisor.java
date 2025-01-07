fa0bb5731dfcd7d081d3764db49fa1047465bf1d
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2025-01-07 21:28:01 +0900

    Title: 최대공약수 구하기, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250107/최대공약수 구하기/find-the-greatest-common-divisor.java
+++ 250107/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void commonDiviser(int n, int m){
+        int gcd = 0;
+
+        while(n % m == 0){
+           gcd = n % m;
+        }
+        
         System.out.println(m % n);
     }
 
